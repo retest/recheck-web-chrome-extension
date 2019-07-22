eb1d8ae0317d560094a90514f4989a25b92f05ac
Jeremias Roessler <roessler@retest.de>
2019-07-22 23:52:42 +0200

    Prompt for a name for the check and pass that as well

--- content.js
+++ content.js
@@ -2,10 +2,22 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if ( request.message === "clicked_recheck-web" ) {
+    	var name = prompt("Please enter the name of the check:", document.title);
+    	if (name == null || name == "") {
+    		return;
+    	} 
     	var request = new XMLHttpRequest();
     	request.open("POST", "http://localhost:8080/api/v0.1.0/paths-webdata-mapping", true);
     	request.setRequestHeader('Content-Type', 'application/json');
-    	request.send(JSON.stringify(allElements));
+    	request.onreadystatechange = function() {
+    	    if (request.readyState === 4) {
+    	      alert(request.response);
+    	    }
+    	}
+    	request.send(JSON.stringify({
+    		"allElements": allElements,
+    		"name": name
+    		}));
     }
   }
 );
