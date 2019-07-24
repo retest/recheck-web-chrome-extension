53c888dea1030ed90aaa8aad008ab3cff0ccee13
Jeremias Roessler <roessler@retest.de>
2019-07-24 19:28:57 +0200

    Sending the actual request from the background.js to avoid CORB
    Cross-Origin Read Blocking (CORB): https://www.chromestatus.com/feature/5629709824032768

--- background.js
+++ background.js
@@ -24,13 +24,31 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	if (request.message === 'recheck-web_login') {
 		chrome.tabs.sendMessage(activeTabId, {
-			'message' : 'clicked_recheck-web',
-			'authenticated' : request.authenticated,
-			'token' : request.token,
-			'refreshToken' : request.refreshToken,
-			'subject' : request.subject,
-			'realmAccess' : request.realmAccess,
-			'resourceAccess' : request.resourceAccess
+			'message' : 'recheck-web_clicked'
+		}, function(response) {
+			var xhr = new XMLHttpRequest();
+			xhr.open('POST', 'http://localhost:8080/api/v1.3.0/paths-webdata-mapping', true);
+			xhr.setRequestHeader('Content-Type', 'application/json');
+			xhr.setRequestHeader('Authorization', 'Bearer ' + request.token);
+			xhr.onreadystatechange = function() {
+				if (xhr.readyState === 4) {
+					if (xhr.status == 200) {
+						alert(xhr.response);
+					} else {
+						alert('Request returned status : ' + xhr.response);
+					}
+				}
+			}
+			xhr.send(JSON.stringify({
+				'allElements' : JSON.parse(response.allElements),
+				'name' : response.name,
+				'authenticated' : request.authenticated,
+				'token' : request.token,
+				'refreshToken' : request.refreshToken,
+				'subject' : request.subject,
+				'realmAccess' : request.realmAccess,
+				'resourceAccess' : request.resourceAccess
+			}));
 		});
 	}
 });
--- content.js
+++ content.js
@@ -1,34 +1,14 @@
 // content.js
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if ( request.message === 'clicked_recheck-web' ) {
-    	var name = prompt('Please enter the name of the check: ', document.title);
-    	if (name == null || name == '') {
-    		return;
-    	} 
-    	var xhr = new XMLHttpRequest();
-    	xhr.open('POST', 'http://localhost:8080/api/v1.3.0/paths-webdata-mapping', true);
-    	xhr.setRequestHeader('Content-Type', 'application/json');
-    	xhr.setRequestHeader('Authorization', 'Bearer ' + request.token);
-    	xhr.onreadystatechange = function() {
-    	    if (xhr.readyState === 4) {
-    	    	if (xhr.status == 200) {
-    	    		alert(xhr.response);
-    	    	} else {
-    	    		alert('Request returned status : ' + xhr.response);
-    	    	}
-    	    }
-    	}
-    	xhr.send(JSON.stringify({
-    		'allElements': allElements,
-    		'name': name,
-			'authenticated' : request.authenticated,
-			'token' : request.token,
-			'refreshToken' : request.refreshToken,
-			'subject' : request.subject,
-			'realmAccess' : request.realmAccess,
-			'resourceAccess' : request.resourceAccess
-    	}));
-    }
-  }
-);
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+	if (request.message === 'recheck-web_clicked') {
+		var name = prompt('Please enter the name of the check: ',
+				document.title);
+		if (name == null || name == '') {
+			return;
+		}
+		sendResponse({
+			'allElements' : JSON.stringify(allElements),
+			'name' : name
+		});
+	}
+});
