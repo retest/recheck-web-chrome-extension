66bcce6e3587d739d395cd49f6e5a554cfd578f7
Jeremias Roessler <roessler@retest.de>
2019-07-26 14:52:50 +0200

    Do not send sensitive keyCloak data to everybody, but only to the recheck extension

--- login.js
+++ login.js
@@ -1,3 +1,6 @@
+// Adapt that if developing the extension and loading unpacked...
+const recheckWebExtensionId = 'ifbcdobnjihilgldbjeomakdaejhplii';
+
 var keycloak = Keycloak({
 // TODO url: 'https://sso.prod.cloud.retest.org/auth',
 	url: 'https://sso.dev.cloud.retest.org/auth',
@@ -5,10 +8,10 @@ var keycloak = Keycloak({
     clientId: 'babelfish'
 });
 
-window.addEventListener("load", function(event) {
+window.addEventListener('load', function(event) {
     keycloak.init({ onLoad: 'login-required' })
         .success(function(){
-	        chrome.runtime.sendMessage({
+        	chrome.runtime.sendMessage(recheckWebExtensionId, {
 	        	'message': 'recheck-web_login',
 	        	'token': keycloak.token
 	        });
