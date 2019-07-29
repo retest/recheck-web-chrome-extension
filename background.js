668eb7bff2f422e4da6756eb27616346ffebdc2d
Jeremias Roessler <roessler@retest.de>
2019-07-29 23:36:27 +0200

    Resize the image to reduce payload size...

--- background.js
+++ background.js
@@ -5,77 +5,70 @@ const REPORT_DASHBOARD_URL = 'http://garkbit.dev.cloud.retest.org/dashboard';
 const RESPONSE_GOLDEN_MASTER_CREATED = 'recheck-web-Golden-Master-created';
 const RESPONSE_REPORT_CREATED = 'recheck-web-Report-created';
 
+const ERROR_MSG = 'There was an error in the recheck plugin. Please refresh this page and try again.\n\nIf it still does not work, please consider reporting a bug at\nhttps://github.com/retest/recheck-web-chrome-extension/issues\n\nThank you!';
+
 var activeWindowId;
 var activeTab;
 var activeTabId;
 
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-	chrome.windows.getCurrent({}, function(window){
-		activeWindowId = window.id;
-		// persist tabId of activeTab
-		chrome.tabs.query({
-			active : true,
-			currentWindow : true
-		}, function(tabs) {
-			activeTab = tabs[0];
-			activeTabId = activeTab.id;
-			chrome.windows.create({
-				'url' : 'login.html',
-				'left' : 100,
-				'top' : 0,
-				'width' : 1000,
-				'height' : 870
-			});
-		});
-	});
-});
-
-function captureScreenshots(callback) {
+function requestScreenshots() {
 	chrome.tabs.captureVisibleTab(activeWindowId, {
 		format : 'jpeg',
 		quality : 50
 	}, function(dataUrl) {
-		callback(dataUrl);
+		chrome.tabs.sendMessage(activeTabId, {
+			'message' : 'recheck-web_resize_img',
+			'dataUrl' : dataUrl,
+			'width' : 1200,
+			'height' : 800
+		});
 	});
 }
 
-function requestData(token) {
+function requestData() {
 	chrome.tabs.sendMessage(activeTabId, {
 		'message' : 'recheck-web_clicked'
 	}, function(response) {
 		if (response == null) {
-			alert('There was an error in the recheck plugin. Please refresh this page and try again.\n\nIf it still does not work, please consider reporting a bug at\nhttps://github.com/retest/recheck-web-chrome-extension/issues\n\nThank you!');
+			alert(ERROR_MSG);
 			return;
 		}
-		sendData(response, token);
+		data = response;
 	});
 }
 
-function sendData(request, token) {
-	captureScreenshots(function(dataUrl) {
-		var name = prompt('Please enter the name of the check: ', request.title);
-		if (name == null || name == '') {
-			return;
-		}
-		var xhr = new XMLHttpRequest();
-		xhr.open('POST', MAPPING_SERVICE_URL, true);
-		xhr.setRequestHeader('Content-Type', 'application/json');
-		xhr.setRequestHeader('Authorization', 'Bearer ' + token);
-		xhr.onreadystatechange = function() {
-			handleServerResponse(xhr.readyState, xhr.status, xhr.response, name);
-		}
-		xhr.send(JSON.stringify({
-			'allElements' : JSON.parse(request.allElements),
-			'screenshot' : dataUrl,
-			'name' : name,
-			'title' : request.title,
-			'url' : request.url,
-			'os' : request.os,
-			'browser' : request.browser,
-			'screensize' : request.screensize
-		}));
+function requestLogin() {
+	chrome.windows.create({
+		'url' : 'login.html',
+		'left' : 100,
+		'top' : 0,
+		'width' : 1000,
+		'height' : 870
 	});
+}
+
+function sendData(request, dataUrl, token) {
+	var name = prompt('Please enter the name of the check: ', request.title);
+	if (name == null || name == '') {
+		return;
+	}
+	var xhr = new XMLHttpRequest();
+	xhr.open('POST', MAPPING_SERVICE_URL, true);
+	xhr.setRequestHeader('Content-Type', 'application/json');
+	xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+	xhr.onreadystatechange = function() {
+		handleServerResponse(xhr.readyState, xhr.status, xhr.response, name);
+	}
+	xhr.send(JSON.stringify({
+		'allElements' : JSON.parse(request.allElements),
+		'screenshot' : dataUrl,
+		'name' : name,
+		'title' : request.title,
+		'url' : request.url,
+		'os' : request.os,
+		'browser' : request.browser,
+		'screensize' : request.screensize
+	}));
 }
 
 function handleServerResponse(readyState, status, response, name) {
@@ -88,7 +81,9 @@ function handleServerResponse(readyState, status, response, name) {
 					'url' : REPORT_DASHBOARD_URL
 				});
 			} else {
-				alert('Error interacting with the retest server:\n\n' + response + '\n\nPlease refresh this page and try again. If it still does not work, please contact support: support@retest.de');
+				alert('Error interacting with the retest server:\n\n'
+						+ response
+						+ '\n\nPlease refresh this page and try again. If it still does not work, please contact support: support@retest.de');
 			}
 		} else if (status == 403) {
 			alert('Something is wrong with your access rights.\nPlease contact support: support@retest.de');
@@ -98,8 +93,40 @@ function handleServerResponse(readyState, status, response, name) {
 	}
 }
 
+// when clicked
+// requestLogin
+// when login
+var token;
+// requestScreenshot
+var screenshot;
+// requestData
+var data;
+// when both
+// send all
+
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+	chrome.windows.getCurrent({}, function(window) {
+		activeWindowId = window.id;
+		// persist tabId of activeTab
+		chrome.tabs.query({
+			active : true,
+			currentWindow : true
+		}, function(tabs) {
+			activeTab = tabs[0];
+			activeTabId = activeTab.id;
+			requestLogin();
+		});
+	});
+});
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	if (request.message === 'recheck-web_login') {
-		requestData(request.token);
+		token = request.token;
+		requestData();
+		requestScreenshots();
+	}
+	if (request.message === 'recheck-web_resize_img') {
+		sendData(data, request.dataUrl, token);
 	}
 });
--- content.js
+++ content.js
@@ -16,6 +16,32 @@ function getScreensize() {
 	}
 }
 
+function resizeDataUrl(dataUrl, wantedWidth, wantedHeight, callback) {
+	var img = document.createElement('img');
+
+	img.onload = function() {
+		// We create a canvas and get its context.
+		var canvas = document.createElement('canvas');
+		var ctx = canvas.getContext('2d');
+
+		// We set the dimensions at the wanted size.
+		canvas.width = wantedWidth;
+		canvas.height = wantedHeight;
+
+		// We resize the image with the canvas method drawImage();
+		ctx.drawImage(this, 0, 0, wantedWidth, wantedHeight);
+
+		var newDataUrl = canvas.toDataURL();
+		chrome.runtime.sendMessage({
+			'message' : 'recheck-web_resize_img',
+			'dataUrl' : newDataUrl
+		});
+	};
+
+	// We put the Data URI in the image's src attribute
+	img.src = dataUrl;
+}
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	if (request.message === 'recheck-web_clicked') {
 		var htmlNode = document.getElementsByTagName("html")[0];
@@ -24,7 +50,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 			"//html[1]" : html
 		});
 		sendResponse({
-			'message' : 'recheck-web-send_data',
+			'message' : 'recheck-web_send_data',
 			'allElements' : JSON.stringify(allElements),
 			'title' : document.title,
 			'url' : window.location.href,
@@ -32,5 +58,9 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 			'browser' : getBrowser(),
 			'screensize' : getScreensize()
 		});
+	}
+	if (request.message === 'recheck-web_resize_img') {
+		resizeDataUrl(request.dataUrl, request.width, request.height);
+		sendResponse();
 	}
 });
