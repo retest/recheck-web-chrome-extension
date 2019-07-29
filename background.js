a14c2dcb1321735b193d95141174ed150c195bb2
Jeremias Roessler <roessler@retest.de>
2019-07-29 17:36:36 +0200

    Use the chrome internal captureTab functionality instead of dom-to-img

--- background.js
+++ background.js
@@ -5,61 +5,73 @@ const REPORT_DASHBOARD_URL = 'http://garkbit.dev.cloud.retest.org/dashboard';
 const RESPONSE_GOLDEN_MASTER_CREATED = 'recheck-web-Golden-Master-created';
 const RESPONSE_REPORT_CREATED = 'recheck-web-Report-created';
 
+var activeWindowId;
+var activeTab;
 var activeTabId;
-var token;
-var timeoutHandle;
 
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
-	// persist tabId of activeTab
-	chrome.tabs.query({
-		active : true,
-		currentWindow : true
-	}, function(tabs) {
-		var activeTab = tabs[0];
-		activeTabId = activeTab.id;
-		chrome.windows.create({
-			'url' : 'login.html',
-			'left' : 100,
-			'top' : 0,
-			'width' : 1000,
-			'height' : 870
+	chrome.windows.getCurrent({}, function(window){
+		activeWindowId = window.id;
+		// persist tabId of activeTab
+		chrome.tabs.query({
+			active : true,
+			currentWindow : true
+		}, function(tabs) {
+			activeTab = tabs[0];
+			activeTabId = activeTab.id;
+			chrome.windows.create({
+				'url' : 'login.html',
+				'left' : 100,
+				'top' : 0,
+				'width' : 1000,
+				'height' : 870
+			});
 		});
 	});
 });
 
-function requestData() {
-	chrome.tabs.sendMessage(activeTabId, {
-		'message' : 'recheck-web_clicked'
-	}, function(){
-		timeoutHandle = setTimeout(function(){ 
-			alert('There was an error in the recheck plugin. Please refresh this page and try again.\n\nIf it still does not work, please consider reporting a bug at\nhttps://github.com/retest/recheck-web-chrome-extension/issues\n\nThank you!'); 
-		}, 10000);
+function captureScreenshots(callback) {
+	chrome.tabs.captureVisibleTab(activeWindowId, {
+		format : 'jpeg',
+		quality : 50
+	}, function(dataUrl) {
+		callback(dataUrl);
 	});
 }
 
-function sendData(request) {
-	var name = prompt('Please enter the name of the check: ', request.title);
-	if (name == null || name == '') {
-		return;
-	}
-	var xhr = new XMLHttpRequest();
-	xhr.open('POST', MAPPING_SERVICE_URL, true);
-	xhr.setRequestHeader('Content-Type', 'application/json');
-	xhr.setRequestHeader('Authorization', 'Bearer ' + token);
-	xhr.onreadystatechange = function() {
-		handleServerResponse(xhr.readyState, xhr.status, xhr.response, name);
-	}
-	xhr.send(JSON.stringify({
-		'allElements' : JSON.parse(request.allElements),
-		'screenshot' : request.screenshot,
-		'name' : name,
-		'title' : request.title,
-		'url' : request.url,
-		'os' : request.os,
-		'browser' : request.browser,
-		'screensize' : request.screensize
-	}));
+function requestData(token) {
+	chrome.tabs.sendMessage(activeTabId, {
+		'message' : 'recheck-web_clicked'
+	}, function(response) {
+		sendData(response, token);
+	});
+}
+
+function sendData(request, token) {
+	captureScreenshots(function(dataUrl) {
+		var name = prompt('Please enter the name of the check: ', request.title);
+		if (name == null || name == '') {
+			return;
+		}
+		var xhr = new XMLHttpRequest();
+		xhr.open('POST', MAPPING_SERVICE_URL, true);
+		xhr.setRequestHeader('Content-Type', 'application/json');
+		xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+		xhr.onreadystatechange = function() {
+			handleServerResponse(xhr.readyState, xhr.status, xhr.response, name);
+		}
+		xhr.send(JSON.stringify({
+			'allElements' : JSON.parse(request.allElements),
+			'screenshot' : dataUrl,
+			'name' : name,
+			'title' : request.title,
+			'url' : request.url,
+			'os' : request.os,
+			'browser' : request.browser,
+			'screensize' : request.screensize
+		}));
+	});
 }
 
 function handleServerResponse(readyState, status, response, name) {
@@ -84,11 +96,6 @@ function handleServerResponse(readyState, status, response, name) {
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	if (request.message === 'recheck-web_login') {
-		token = request.token;
-		requestData();
-	}
-	if (request.message === 'recheck-web-send_data') {
-		clearTimeout(timeoutHandle);
-		sendData(request);
+		requestData(request.token);
 	}
 });
--- content.js
+++ content.js
@@ -9,44 +9,28 @@ function getBrowser() {
 }
 
 function getScreensize() {
-    if (screen.width) {
-        width = (screen.width) ? screen.width : '';
-        height = (screen.height) ? screen.height : '';
-        return '' + width + 'x' + height;
-    }
-}
-
-async function captureScreenshot(callback) {
-	let promise = new Promise((resolve, reject) => {
-		  domtoimage.toJpeg(document.body, { quality: 0.3 })
-		  .then(function (dataUrl) {
-			  callback(dataUrl);
-		  })
-		  .catch(function (error) {
-			  callback('ERROR: Error creating screenshot for ' + window.location.href);
-		  });
-	});
+	if (screen.width) {
+		width = (screen.width) ? screen.width : '';
+		height = (screen.height) ? screen.height : '';
+		return '' + width + 'x' + height;
+	}
 }
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	if (request.message === 'recheck-web_clicked') {
-		captureScreenshot(function(dataUrl) {
-			var htmlNode = document.getElementsByTagName("html")[0];
-			var html = transform(htmlNode);
-			var allElements = mapElement(htmlNode, "//html[1]", {
-				"//html[1]": html
-			});
-			chrome.runtime.sendMessage({
-				'message' : 'recheck-web-send_data',
-				'allElements' : JSON.stringify(allElements),
-				'screenshot' : dataUrl,
-				'title' : document.title,
-				'url' : window.location.href,
-				'os' : getOs(),
-				'browser' : getBrowser(),
-				'screensize' : getScreensize()
-			});
+		var htmlNode = document.getElementsByTagName("html")[0];
+		var html = transform(htmlNode);
+		var allElements = mapElement(htmlNode, "//html[1]", {
+			"//html[1]" : html
 		});
-		sendResponse();
+		sendResponse({
+			'message' : 'recheck-web-send_data',
+			'allElements' : JSON.stringify(allElements),
+			'title' : document.title,
+			'url' : window.location.href,
+			'os' : getOs(),
+			'browser' : getBrowser(),
+			'screensize' : getScreensize()
+		});
 	}
 });
