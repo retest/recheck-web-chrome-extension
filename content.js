d3b579818aaa58300f25f66f97cb361fb3dcea18
Jeremias Roessler <roessler@retest.de>
2019-08-01 22:32:40 +0200

    Guard against content being injected several times...

--- content.js
+++ content.js
@@ -1,59 +1,64 @@
 // content.js
 
-var WANTED_WIDTH = 800;
+if (!alreadyInjected) {
 
-function getOs() {
-	return navigator.oscpu;
-}
+	var alreadyInjected = true;
 
-function getBrowser() {
-	return navigator.vendor + ' ' + navigator.vendorSub;
-}
+	var WANTED_WIDTH = 800;
 
-function resizeDataUrl(dataUrl) {
-	var img = document.createElement('img');
-
-	img.onload = function() {
-		var canvas = document.createElement('canvas');
-		var ctx = canvas.getContext('2d');
-
-		canvas.width = WANTED_WIDTH;
-		canvas.height = img.height * (WANTED_WIDTH / img.width);
-
-		ctx.drawImage(this, 0, 0, canvas.width, canvas.height);
-
-		var newDataUrl = canvas.toDataURL();
-		chrome.runtime.sendMessage({
-			'message' : 'recheck-web_resize_img',
-			'dataUrl' : newDataUrl
-		});
-	};
-
-	img.src = dataUrl;
-}
-
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-	if (request.message === 'recheck-web_clicked') {
-		var htmlNode = document.getElementsByTagName("html")[0];
-		var html = transform(htmlNode);
-		var allElements = mapElement(htmlNode, "//html[1]", {
-			"//html[1]" : html
-		});
-		sendResponse({
-			'message' : 'recheck-web_send_data',
-			'allElements' : JSON.stringify(allElements),
-			'title' : document.title,
-			'url' : window.location.href,
-			'os' : getOs(),
-			'browser' : getBrowser(),
-			'screenWidth' : screen.width,
-			'screenHeight' : screen.height
-		});
+	function getOs() {
+		return navigator.oscpu;
 	}
-	if (request.message === 'recheck-web_resize_img') {
-		request.dataUrls.forEach(function(dataUrl) {
-			resizeDataUrl(dataUrl);
-		});
-		sendResponse();
+
+	function getBrowser() {
+		return navigator.vendor + ' ' + navigator.vendorSub;
 	}
-});
+
+	function resizeDataUrl(dataUrl) {
+		var img = document.createElement('img');
+
+		img.onload = function() {
+			var canvas = document.createElement('canvas');
+			var ctx = canvas.getContext('2d');
+
+			canvas.width = WANTED_WIDTH;
+			canvas.height = img.height * (WANTED_WIDTH / img.width);
+
+			ctx.drawImage(this, 0, 0, canvas.width, canvas.height);
+
+			var newDataUrl = canvas.toDataURL();
+			chrome.runtime.sendMessage({
+				'message' : 'recheck-web_resize_img',
+				'dataUrl' : newDataUrl
+			});
+		};
+
+		img.src = dataUrl;
+	}
+
+	chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+		if (request.message === 'recheck-web_clicked') {
+			var htmlNode = document.getElementsByTagName("html")[0];
+			var html = transform(htmlNode);
+			var allElements = mapElement(htmlNode, "//html[1]", {
+				"//html[1]" : html
+			});
+			sendResponse({
+				'message' : 'recheck-web_send_data',
+				'allElements' : JSON.stringify(allElements),
+				'title' : document.title,
+				'url' : window.location.href,
+				'os' : getOs(),
+				'browser' : getBrowser(),
+				'screenWidth' : screen.width,
+				'screenHeight' : screen.height
+			});
+		}
+		if (request.message === 'recheck-web_resize_img') {
+			request.dataUrls.forEach(function(dataUrl) {
+				resizeDataUrl(dataUrl);
+			});
+			sendResponse();
+		}
+	});
+}
