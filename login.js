07d67edbbe753780666f64af64eec8fdc5b5eaf3
Jeremias Roessler <roessler@retest.de>
2019-08-06 09:08:27 +0200

    Improve formatting

--- login.js
+++ login.js
@@ -1,20 +1,19 @@
 var keycloak = Keycloak({
-	url: 'https://sso.prod.cloud.retest.org/auth',
-    realm: 'customer',
-    clientId: 'babelfish'
+	url : 'https://sso.prod.cloud.retest.org/auth',
+	realm : 'customer',
+	clientId : 'babelfish'
 });
 
 window.addEventListener("load", function(event) {
-    keycloak.init({ onLoad: 'login-required' })
-        .success(function(){
-        	console.log("Sending login info.")
-	        chrome.runtime.sendMessage({
-	        	'message': 'recheck-web_login',
-	        	'token': keycloak.token
-	        });
-			window.close();
-        })
-        .error(function(errorData) {
-            document.getElementById('messages').innerHTML = '<b>Failed to load data. Error: ' + JSON.stringify(errorData) + '</b>';
-        });
-});
+	keycloak.init({
+		onLoad : 'login-required'
+	}).success(function() {
+		console.log("Sending login info.")
+		chrome.runtime.sendMessage({
+			'message' : 'recheck-web_login',
+			'token' : keycloak.token
+		});
+		window.close();
+	}).error(function(errorData) {
+		document.getElementById('messages').innerHTML = '<b>Failed to load data. Error: ' + JSON.stringify(errorData) + '</b>';
+	});
